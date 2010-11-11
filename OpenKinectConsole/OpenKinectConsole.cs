c80dfd3f24b0ab01e58a03faff6123238c578120
Joshua Blake <joshblake@gmail.com>
2010-11-11 02:14:28 -0500

    Added initial OpenKinect libraries

---
+++ OpenKinectConsole/OpenKinectConsole.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibUsbDotNet.Main;
+using LibUsbDotNet;
+using OpenKinect;
+using System.Threading;
+
+namespace OpenKinectConsole
+{
+    class OpenKinectConsole
+    {
+        static void Main(string[] args)
+        {
+            ExerciseMotor();
+
+            
+            // Wait for user input..
+            Console.WriteLine("done");
+            Console.ReadKey();
+
+        }
+
+        private static void ExerciseMotor()
+        {
+            KinectMotor motor = new KinectMotor();
+
+            motor.SetTilt(50);
+            motor.SetLED(KinectLEDStatus.Red);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            motor.SetTilt(255);
+            motor.SetLED(KinectLEDStatus.BlinkingYellow);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            motor.SetTilt(128);
+            motor.SetLED(KinectLEDStatus.AlternateRedGreen);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            motor.SetLED(KinectLEDStatus.Green);
+        }
+    }
+}
