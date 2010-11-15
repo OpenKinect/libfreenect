92ab7f37d08abbd6b610188edaca96a1db842311
Hector Martin <hector@marcansoft.com>
2010-11-15 13:27:23 +0100

    Add new license headers and files
    Signed-off-by: Hector Martin <hector@marcansoft.com>

--- c/include/libfreenect.h
+++ c/include/libfreenect.h
@@ -1,12 +1,28 @@
-/*  libfreenect - an open source Kinect driver
-
-Copyright (C) 2010  Hector Martin "marcan" <hector@marcansoft.com>
-
-This code is licensed to you under the terms of the GNU GPL, version 2 or version 3;
-see:
- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
- http://www.gnu.org/licenses/gpl-3.0.txt
-*/
+/*
+ * This file is part of the OpenKinect Project. http://www.openkinect.org
+ *
+ * Copyright (c) 2010 individual OpenKinect contributors. See the CONTRIB file
+ * for details.
+ *
+ * This code is licensed to you under the terms of the Apache License, version
+ * 2.0, or, at your option, the terms of the GNU General Public License,
+ * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
+ * or the following URLs:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * If you redistribute this file in source form, modified or unmodified, you
+ * may:
+ *   1) Leave this header intact and distribute it under the same terms,
+ *      accompanying it with the APACHE20 and GPL20 files, or
+ *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
+ *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
+ * In all cases you must keep the copyright notice intact and include a copy
+ * of the CONTRIB file.
+ *
+ * Binary distributions must follow the binary distribution requirements of
+ * either License.
+ */
 
 #ifndef LIBFREENECT_H
 #define LIBFREENECT_H
