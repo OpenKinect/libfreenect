4cc3bc8e275acfe4c93e41cec3c25d1942c66342
Steven Lovegrove <stevenlovegrove@gmail.com>
2010-11-18 16:20:45 +0000

    Modified CSharp Tilt sample to demonstrate horizon following
    The Kinect tilt control is relative to the horizon. This update
    modifies the CSharp sample to demonstrate this.
    
    Signed-off-by: Steven Lovegrove <stevenlovegrove@gmail.com>

--- csharp/OpenKinectConsole/OpenKinectConsole.cs
+++ csharp/OpenKinectConsole/OpenKinectConsole.cs
@@ -11,15 +11,41 @@ namespace OpenKinectConsole
 {
     class OpenKinectConsole
     {
+        static void Usage()
+        {
+            Console.WriteLine(
+               "Usage:\n  " +
+                System.IO.Path.GetFileName(Environment.GetCommandLineArgs()[0]) +
+                " [--horizon | (pos)]"
+            );
+        }
+
         static void Main(string[] args)
         {
             KinectMotor motor = new KinectMotor();
 
             if( args.Length ==1 )
             {
-                sbyte pos = sbyte.Parse(args[0]);
-                motor.SetTilt(pos);
+                if( args[0] == "--horizon" )
+                {
+                    // Demonstrate horizon following
+                    Console.WriteLine("Press any key to exit");
+                    while(Console.KeyAvailable == false)
+                    {
+                        motor.SetTilt(0);
+                        Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                    }
+                }else{
+                    // User defined value
+                    sbyte pos = sbyte.Parse(args[0]);
+                    motor.SetTilt(pos);
+                }
             }else{
+                Usage();
+
+                Console.WriteLine("Demo sequence started");
+
+                // Sample Sequence
                 ExerciseMotor(motor,+50);
                 ExerciseMotor(motor,-50);
                 ExerciseMotor(motor,+60);
@@ -28,7 +54,6 @@ namespace OpenKinectConsole
             }
 
             Console.WriteLine("done");
-            Console.ReadKey();
         }
 
         private static void ExerciseMotor(KinectMotor motor, sbyte pos)
