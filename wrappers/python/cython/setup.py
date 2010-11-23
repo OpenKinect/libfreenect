6c28612b67839803cfd4a709e7aec069904e3363
Andrew <amiller@dappervision.com>
2010-11-23 14:26:53 -0500

    Fixed the setup.py tor efer to the new builds

--- wrappers/python/cython/setup.py
+++ wrappers/python/cython/setup.py
@@ -3,9 +3,9 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules = [Extension("freenect", ["freenect.pyx"],
-                         extra_objects=["../../c/build/lib/libfreenect.a"],
+                         extra_objects=["../../../c/build/lib/libfreenect.a"],
                          libraries=['usb-1.0'],
-                         extra_compile_args=['-fPIC', '-I', '../../c/include/',
+                         extra_compile_args=['-fPIC', '-I', '../../../include/',
                                              '-I', '/usr/include/libusb-1.0/',
                                              '-I', '/usr/local/include/libusb-1.0',
                                              '-I', '/usr/local/include'])]
