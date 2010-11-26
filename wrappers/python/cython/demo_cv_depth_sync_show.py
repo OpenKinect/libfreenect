cc4113b864ac2b79ac4ab18e77ce10f550743ddd
Brandyn A. White <bwhite@dappervision.com>
2010-11-26 01:54:26 -0500

    Combined cython_sync and cython Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- wrappers/python/cython/demo_cv_depth_sync_show.py
+++ wrappers/python/cython/demo_cv_depth_sync_show.py
@@ -1,11 +1,10 @@
-import freenect_sync as freenect
+import freenect
 import cv
 import numpy as np
 
 
 cv.NamedWindow('Depth')    
 while 1:
-    depth, timestamp = freenect.get_depth_np()
+    depth, timestamp = freenect.sync_get_depth_np()
     cv.ShowImage('Depth', depth.astype(np.uint8))
     cv.WaitKey(10)
-
