05d8a4395811bb92e9dbb460b16fb322826437e0
Brandyn A. White <bwhite@dappervision.com>
2010-11-25 22:56:46 -0500

    Changed to new interface to allow for getting timestamps Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- wrappers/c_sync/libfreenect_sync.h
+++ wrappers/c_sync/libfreenect_sync.h
@@ -1,10 +1,25 @@
 #ifndef FREENECT_SYNC_H
 #define FREENECT_SYNC_H
 
-#define DEPTH_BYTES 614400 // 480 * 640 * 2
-#define RGB_BYTES 921600  // 480 * 640 * 3
-char *freenect_get_depth();
-char *freenect_get_rgb();
-void freenect_sync_stop();
+int freenect_sync_get_rgb(char **rgb, uint32_t *timestamp);
+/*  Synchronous rgb function, starts the runloop if it isn't running
 
+    Args:
+        rgb: Populated with a pointer to a RGB buffer (of size RGB_BYTES)
+        timestamp: Populated with a pointer to the associated timestamp
+
+    Returns:
+        Nonzero on error.
+*/
+int freenect_sync_get_depth(char **depth, uint32_t *timestamp);
+/*  Synchronous depth function, starts the runloop if it isn't running
+
+    Args:
+        depth: Populated with a pointer to a depth buffer (of size DEPTH_BYTES)
+        timestamp: Populated with a pointer to the associated timestamp
+
+    Returns:
+        Nonzero on error.
+*/
+void freenect_sync_stop();
 #endif
