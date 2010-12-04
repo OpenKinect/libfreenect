e095d8d32791f53991d27899640e00953cc3991e
Andrew <amiller@dappervision.com>
2010-12-04 01:34:15 -0500

    Added RGB color alignment to the GlPclView demo. It's not perfect, but it's a reasonable start using earlier published values.
    Signed-off-by: Andrew <amiller@dappervision.com>

--- examples/glpclview.c
+++ examples/glpclview.c
@@ -3,6 +3,8 @@
  *
  * Copyright (c) 2010 individual OpenKinect contributors. See the CONTRIB file
  * for details.
+ * 
+ * Andrew Miller <amiller@dappervision.com>
  *
  * This code is licensed to you under the terms of the Apache License, version
  * 2.0, or, at your option, the terms of the GNU General Public License,
@@ -48,18 +50,33 @@ float zoom = 1;         // zoom factor
 int color = 1;          // Use the RGB texture or just draw it as color
 
 // Do the projection from u,v,depth to X,Y,Z directly in an opengl matrix
+// These numbers come from a combination of the ros kinect_node wiki, and
+// nicolas burrus' posts.
 void LoadVertexMatrix() 
 {
   float f = 590.0f;
   float a = -0.0030711f;
   float b = 3.3309495f;
-  float cx = 320.0f;
+  float cx = 340.0f;
   float cy = 240.0f;
   float mat[4][4] = {
     {1/f,     0,  0, 0},
     {0,    -1/f,  0, 0},
     {0,       0,  0, a},
     {-cx/f,cy/f, -1, b}};
+  glMultMatrixf(mat);
+}
+
+
+// This matrix comes from a combination of nicolas burrus's calibration post
+// and some python code I haven't documented yet.
+void LoadRGBMatrix()
+{
+  float mat[4][4] = {
+    {  5.34866271e+02,   3.89654806e+00,   0.00000000e+00,   1.74704200e-02},
+    { -4.70724694e+00,  -5.28843603e+02,   0.00000000e+00,  -1.22753400e-02},
+    { -3.19670762e+02,  -2.60999685e+02,   0.00000000e+00,  -9.99772000e-01},
+    { -6.98445586e+00,   3.31139785e+00,   0.00000000e+00,   1.09167360e-02}};
   glMultMatrixf(mat);
 }
 
@@ -95,15 +112,12 @@ void DrawGLScene()
   
   static unsigned int indices[480][640];
   static short xyz[480][640][3];
-  static float uv[480][640][2];
   int i,j;
   for (i = 0; i < 480; i++) {
     for (j = 0; j < 640; j++) {
       xyz[i][j][0] = j;
       xyz[i][j][1] = i;
       xyz[i][j][2] = depth[i*640+j];
-      uv[i][j][0] = j/480.0f;
-      uv[i][j][1] = i/640.0f;
       indices[i][j] = i*640+j; 
     }
   }
@@ -119,12 +133,21 @@ void DrawGLScene()
   glTranslatef(0,0,1.5);
   
   LoadVertexMatrix();
+  
+  // Set the projection from the XYZ to the texture image
+  glMatrixMode(GL_TEXTURE);
+  glLoadIdentity();
+  glScalef(1/640.0f,1/480.0f,1);
+  LoadRGBMatrix();
+  LoadVertexMatrix();
+  glMatrixMode(GL_MODELVIEW);
+  
   glPointSize(1);
 
   glEnableClientState(GL_VERTEX_ARRAY);
   glVertexPointer(3, GL_SHORT, 0, xyz);
   glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glTexCoordPointer(2, GL_FLOAT, 0, uv);
+  glTexCoordPointer(3, GL_SHORT, 0, xyz);
   
   if (color) glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, gl_rgb_tex);
