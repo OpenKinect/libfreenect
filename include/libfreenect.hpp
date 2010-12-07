f6d8be98b4c003597683aa0b389273faf9b9eb80
Kai Ritterbusch <kai@kai.(none)>
2010-12-07 17:44:24 +0100

    Fixed libfreenect.hpp: check of return values now according to convention
    Signed-off-by: Kai Ritterbusch <kai.ritterbusch@gmx.de>

--- include/libfreenect.hpp
+++ include/libfreenect.hpp
@@ -41,26 +41,28 @@ namespace Freenect {
 		const Noncopyable& operator=( const Noncopyable& );
 	};
 	
-	class FreenectDeviceState {
-		friend class FreenectDevice;
-			FreenectDeviceState(freenect_raw_tilt_state *_state):
-				m_state(_state)
-			{}
-		public:
-			void getAccelerometers(double* x, double* y, double* z) {
-				freenect_get_mks_accel(m_state, x, y, z);
-			}
-			double getTiltDegs() {
-				return freenect_get_tilt_degs(m_state);
-			}
-		private:
-			freenect_raw_tilt_state *m_state;
+	class FreenectTiltState {
+	  friend class FreenectDevice;
+		FreenectTiltState(freenect_raw_tilt_state *_state):
+			m_state(_state), m_code(_state->tilt_status)
+		{}
+	  public:
+		void getAccelerometers(double* x, double* y, double* z) {
+			freenect_get_mks_accel(m_state, x, y, z);
+		}
+		double getTiltDegs() {
+			return freenect_get_tilt_degs(m_state);
+		}
+	  public:
+		freenect_tilt_status_code m_code;
+	  private:
+		freenect_raw_tilt_state *m_state;
 	};
 
 	class FreenectDevice : Noncopyable {
 	  public:
 		FreenectDevice(freenect_context *_ctx, int _index) {
-			if(freenect_open_device(_ctx, &m_dev, _index) != 0) throw std::runtime_error("Cannot open Kinect");
+			if(freenect_open_device(_ctx, &m_dev, _index) < 0) throw std::runtime_error("Cannot open Kinect");
 			freenect_set_user(m_dev, this);
 			freenect_set_video_format(m_dev, FREENECT_VIDEO_RGB);
 			freenect_set_depth_format(m_dev, FREENECT_DEPTH_11BIT);
@@ -68,31 +70,31 @@ namespace Freenect {
 			freenect_set_video_callback(m_dev, freenect_video_callback);
 		}
 		~FreenectDevice() {
-			if(freenect_close_device(m_dev) != 0) throw std::runtime_error("Cannot shutdown Kinect");
+			if(freenect_close_device(m_dev) < 0) throw std::runtime_error("Cannot shutdown Kinect");
 		}
 		void startVideo() {
-			if(freenect_start_video(m_dev) != 0) throw std::runtime_error("Cannot start RGB callback");
+			if(freenect_start_video(m_dev) < 0) throw std::runtime_error("Cannot start RGB callback");
 		}
 		void stopVideo() {
-			if(freenect_stop_video(m_dev) != 0) throw std::runtime_error("Cannot stop RGB callback");
+			if(freenect_stop_video(m_dev) < 0) throw std::runtime_error("Cannot stop RGB callback");
 		}
 		void startDepth() {
-			if(freenect_start_depth(m_dev) != 0) throw std::runtime_error("Cannot start depth callback");
+			if(freenect_start_depth(m_dev) < 0) throw std::runtime_error("Cannot start depth callback");
 		}
 		void stopDepth() {
-			if(freenect_stop_depth(m_dev) != 0) throw std::runtime_error("Cannot stop depth callback");
+			if(freenect_stop_depth(m_dev) < 0) throw std::runtime_error("Cannot stop depth callback");
 		}
 		void setTiltDegrees(double _angle) {
-			if(freenect_set_tilt_degs(m_dev, _angle) != 0) throw std::runtime_error("Cannot set angle in degrees");
+			if(freenect_set_tilt_degs(m_dev, _angle) < 0) throw std::runtime_error("Cannot set angle in degrees");
 		}
 		void setLed(freenect_led_options _option) {
-			if(freenect_set_led(m_dev, _option) != 0) throw std::runtime_error("Cannot set led");
+			if(freenect_set_led(m_dev, _option) < 0) throw std::runtime_error("Cannot set led");
 		}
 		void updateState() {
-			if (freenect_update_tilt_state(m_dev) != 0) throw std::runtime_error("Cannot update device state");
+			if (freenect_update_tilt_state(m_dev) < 0) throw std::runtime_error("Cannot update device state");
 		}
-		FreenectDeviceState getState() const {
-			return FreenectDeviceState(freenect_get_tilt_state(m_dev));
+		FreenectTiltState getState() const {
+			return FreenectTiltState(freenect_get_tilt_state(m_dev));
 		}
 		// Do not call directly even in child
 		virtual void VideoCallback(void *video, uint32_t timestamp) = 0;
@@ -113,7 +115,7 @@ namespace Freenect {
 	template <class T>class Freenect : Noncopyable {
 	  public:
 		Freenect() : m_stop(false) {
-			if(freenect_init(&m_ctx, NULL) != 0) throw std::runtime_error("Cannot initialize freenect library");
+			if(freenect_init(&m_ctx, NULL) < 0) throw std::runtime_error("Cannot initialize freenect library");
 			if(pthread_create(&m_thread, NULL, pthread_callback, (void*)this) != 0) throw std::runtime_error("Cannot initialize freenect thread");
 		}
 		~Freenect() {
@@ -122,7 +124,7 @@ namespace Freenect {
 			}
 			m_stop = true;
 			pthread_join(m_thread, NULL);
-			if(freenect_shutdown(m_ctx) != 0) throw std::runtime_error("Cannot cleanup freenect library");
+			if(freenect_shutdown(m_ctx) < 0) throw std::runtime_error("Cannot cleanup freenect library");
 		}
 		T& createDevice(int _index) {
 			m_devices.insert(std::make_pair<int, T*>(_index, new T(m_ctx, _index)));
@@ -137,7 +139,7 @@ namespace Freenect {
 		// Do not call directly, thread runs here
 		void operator()() {
 			while(!m_stop) {
-				if(freenect_process_events(m_ctx) != 0) throw std::runtime_error("Cannot process freenect events");
+				if(freenect_process_events(m_ctx) < 0) throw std::runtime_error("Cannot process freenect events");
 			}
 		}
 		static void *pthread_callback(void *user_data) {
