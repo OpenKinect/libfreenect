7c0872f4fab76ea5301e4af478a32e496c932c03
David García Garzón <david.garcia@barcelonamedia.org>
2010-12-27 18:35:31 +0100

    No reallocations and GC stalls in python examples
    Calling as_type creates a temporary np matrix that is
    allocated and deallocated all the time. This makes
    the performance to degenerate after a short time.
    
    This was solved by preallocating a matrix for the
    bgr->rgb inversion and using directly the depth map.
    
    Signed-off-by: David García Garzón <david.garcia@barcelonamedia.org>

--- wrappers/python/demo_cv_sync_multi.py
+++ wrappers/python/demo_cv_sync_multi.py
@@ -6,18 +6,29 @@ import numpy as np
 cv.NamedWindow('Depth')
 cv.NamedWindow('Video')
 ind = 0
+size = (640, 480)
+
+# preallocate temporary images to avoid GC stalls and runtime memory allocs
+rgb=np.zeros((size[1],size[0],3),np.uint8)
+
 while 1:
     print(ind)
     try:
         depth, timestamp = freenect.sync_get_depth(ind)
-        rgb, timestamp = freenect.sync_get_video(ind)
+        bgr, timestamp = freenect.sync_get_video(ind)
     except TypeError:
         ind = 0
         continue
     # maximize dynamic range of the given 11 bits into the used 16 bits
     depth<<=(16-11)
+    # Reordering bgr as rgb
+    # Using the slice directly doesn't work because the underlying buffer 
+    # is used and it does not change.
+    rgb[:] = bgr[:,:,::-1]
     ind += 1
-    cv.ShowImage('Depth', depth.astype(np.uint16))
-    cv.ShowImage('Video', rgb[:, :, ::-1].astype(np.uint8))
+
+    cv.ShowImage('Depth', depth)
+    cv.ShowImage('Video', rgb)
     cv.WaitKey(10)
     freenect.sync_stop()# NOTE: May remove if you have good USB bandwidth
+
