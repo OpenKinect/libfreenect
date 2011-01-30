d7241b7e0223239a39250c9c4092b15443c2a35a
Brandyn A. White <bwhite@dappervision.com>
2011-01-30 13:05:31 -0500

    1. Updated setup.py to check if Cython is available and >= 0.13, if not fall back to the .c sources 2. Added the .c sources generated by Cython Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -1,18 +1,49 @@
 #!/usr/bin/env python
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+import re
 
-ext_modules = [Extension("freenect", ["freenect.pyx"],
+
+def get_cython_version():
+    """
+    Returns:
+        Version as a pair of ints (major, minor)
+
+    Raises:
+        ImportError: Can't load cython or find version
+    """
+    import Cython.Compiler.Main
+    match = re.search('^([0-9]+)\.([0-9]+)',
+                      Cython.Compiler.Main.Version.version)
+    try:
+        return map(int, match.groups())
+    except AttributeError:
+        raise ImportError
+
+# Only use Cython if it is available, else just use the pre-generated files
+try:
+    cython_version = get_cython_version()
+    # Requires Cython version 0.13 and up
+    if cython_version[0] == 0 and cython_version[1] < 13:
+        raise ImportError
+    from Cython.Distutils import build_ext
+    source_ext = '.pyx'
+    cmdclass = {'build_ext': build_ext}
+except ImportError:
+    source_ext = '.c'
+    cmdclass = {}
+
+
+ext_modules = [Extension("freenect", ["freenect" + source_ext],
                          libraries=['usb-1.0', 'freenect', 'freenect_sync'],
-                         runtime_library_dirs=['/usr/local/lib', '/usr/local/lib64', '/usr/lib/'],
+                         runtime_library_dirs=['/usr/local/lib',
+                                               '/usr/local/lib64',
+                                               '/usr/lib/'],
                          extra_compile_args=['-fPIC', '-I', '../../include/',
                                              '-I', '/usr/include/libusb-1.0/',
                                              '-I', '/usr/local/include/libusb-1.0',
                                              '-I', '/usr/local/include',
                                              '-I', '../c_sync/'])]
-setup(
-  name = 'freenect',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
-)
+setup(name='freenect',
+      cmdclass=cmdclass,
+      ext_modules=ext_modules)
