9ffab1959cd38b70d28f30cb76516a0f725ca729
Drew Fisher <drew.m.fisher@gmail.com>
2011-05-29 07:34:35 -0700

    C# wrapper: Modify OpenTK fetch script to set the download output filename.
    Signed-off-by: Drew Fisher <drew.m.fisher@gmail.com>

--- wrappers/csharp/support/fetch_opentk.sh
+++ wrappers/csharp/support/fetch_opentk.sh
@@ -1,5 +1,5 @@
-wget http://sourceforge.net/projects/opentk/files/opentk/opentk-1.0/2010-10-06/opentk-2010-10-06.zip/download
-unzip download
+wget http://sourceforge.net/projects/opentk/files/opentk/opentk-1.0/2010-10-06/opentk-2010-10-06.zip/download -Odownload.zip
+unzip download.zip
 rm -rf download
 cp opentk/Binaries/OpenTK/Release/OpenTK.dll .
 cp opentk/Binaries/OpenTK/Release/OpenTK.GLControl.dll .
