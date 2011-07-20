73f5f569e8ebcda048c43d317a7d0dfc415a413d
Florian Echtler <floe@butterbrot.org>
2011-07-20 15:36:12 +0200

    added minimal registration test

---
+++ examples/regtest.c
@@ -0,0 +1,28 @@
+#include "libfreenect.h"
+#include <stdio.h>
+
+uint16_t  input[640*480];
+uint16_t output[640*480];
+	
+freenect_registration registration;
+
+// main routine
+
+int main() {
+
+	freenect_init_registration(0,&registration);
+
+	FILE* df = fopen("depth.raw","r");
+	fread(input,640*480*2,1,df);
+	fclose(df);
+
+	freenect_apply_registration( &registration, input, output );
+	
+	FILE* cf = fopen("calib.pgm","w+");
+	fprintf(cf,"P5 640 480 65535 ");
+	fwrite(output,640*480*2,1,cf);
+	fclose(cf);
+
+	return 0;
+}
+
