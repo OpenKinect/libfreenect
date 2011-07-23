b0671747a879618a3e7725d9dbc1d8c81ec85185
Alex Weiss <algrs@cacography.net>
2011-07-23 18:19:30 +0200

    Minor code cleanup, removing commented out code.

--- wrappers/ruby/ffi-libfreenect/lib/freenect.rb
+++ wrappers/ruby/ffi-libfreenect/lib/freenect.rb
@@ -48,44 +48,4 @@ module Freenect
       self.depth_mode(ii)
     end
   end
-
-  # def self.lookup_video_format(fmt)
-  #   if fmt.is_a?(Numeric)
-  #     unless (p=::FFI::Freenect.freenect_get_video_mode(self.device)).null?
-  #       frame_mode = FrameMode.new(p)
-  #     else
-  #       raise DeviceError, "freenect_get_video_mode() returned a NULL frame_mode"
-  #     end
-  #   else
-  #     unless (p=::FFI::Freenect.freenect_get_video_mode(self.device)).null?
-  #       frame_mode = FrameMode.new(p)
-  #     else
-  #       raise DeviceError, "freenect_get_video_mode() returned a NULL frame_mode"
-  #     end
-  #   end
-  #   
-  #   return frame_mode[:format][:video_format]
-  # end
-  # 
-  # def self.lookup_video_size(fmt)
-  #   l_fmt = (fmt.is_a?(Numeric) ? FFI::Freenect::VIDEO_FORMATS[fmt] : fmt)
-  #   if l_fmt.nil? or (sz = FFI::Freenect::VIDEO_SIZES[l_fmt]).nil?
-  #     return nil
-  #   else
-  #     return sz
-  #   end
-  # end
-  # 
-  # def self.lookup_depth_format(fmt)
-  #   return (fmt.is_a?(Numeric) ? fmt : FFI::Freenect::DEPTH_FORMATS[fmt])
-  # end
-  # 
-  # def self.lookup_depth_size(fmt)
-  #   l_fmt = (fmt.is_a?(Numeric) ? FFI::Freenect::DEPTH_FORMATS[fmt] : fmt)
-  #   if l_fmt.nil? or (sz = FFI::Freenect::DEPTH_SIZES[l_fmt]).nil?
-  #     return nil
-  #   else
-  #     return sz
-  #   end
-  # end
 end
