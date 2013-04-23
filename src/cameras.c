3ef82585695605d40be9bd0a1dc4a882fbb3a4af
Yannis Gravezas <wizgrav@gmail.com>
2013-04-23 14:35:33 +0300

    Kinect for Windows and Xbox 360 Model 1473 support
    This patch does auto detection of all three models and
    disables motor subdevice for the 2 newer ones. It also
    adds a function to test for the presence of the motor
    and audio devices on newer models.
    
    Signed-off-by: Yannis Gravezas <wizgrav@gmail.com> (wizgrav)

--- src/cameras.c
+++ src/cameras.c
@@ -904,9 +904,9 @@ static int freenect_fetch_zero_plane_info(freenect_device *dev)
 	uint16_t cmd[5] = {0}; // Offset is the only field in this command, and it's 0
 
 	int res;
-	res = send_cmd(dev, 0x04, cmd, 10, reply, 322); //OPCODE_GET_FIXED_PARAMS = 4,
-	if (res != 322) {
-		FN_ERROR("freenect_fetch_zero_plane_info: send_cmd read %d bytes (expected 322)\n", res);
+	res = send_cmd(dev, 0x04, cmd, 10, reply, ctx->zero_plane_res); //OPCODE_GET_FIXED_PARAMS = 4,
+	if (res != ctx->zero_plane_res) {
+		FN_ERROR("freenect_fetch_zero_plane_info: send_cmd read %d bytes (expected %d)\n", res,ctx->zero_plane_res);
 		return -1;
 	}
 
