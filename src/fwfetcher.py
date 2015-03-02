95ae970786fd52b1a4191ba12e0084fcf8b549b5
Benn Snyder <benn.snyder@gmail.com>
2015-03-01 21:48:27 -0700

    Fix capitalization in fwfetcher.py - fixes #435
    Signed-off-by: Benn Snyder <benn.snyder@gmail.com>

--- src/fwfetcher.py
+++ src/fwfetcher.py
@@ -531,11 +531,11 @@ def getFileOrURL(filename, url):
 	return retval
 
 def extractPirsFromZip(systemupdate):
-	print "Extracting $systemupdate/FFFE07DF00000001 from system update file..."
+	print "Extracting $Systemupdate/FFFE07DF00000001 from system update file..."
 	updatefile = StringIO.StringIO(systemupdate)
 	z = zipfile.ZipFile(updatefile)
 	#print z.namelist()
-	pirs = z.open("$systemupdate/FFFE07DF00000001").read()
+	pirs = z.open("$Systemupdate/FFFE07DF00000001").read()
 	print "done."
 	return pirs
 
