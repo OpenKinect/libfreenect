569e8b1a286dcd2388ce72f67b5b68aa4e689cac
Caleb Szalacinski <opensource@skiboy.net>
2018-01-13 15:43:01 -0600

    Changes needed to build proxynect on Ubuntu 16.04.  Confirmed working.  2 instances of freenect-tiltdemo are able to concurrently access the Kinect.
    Signed-off-by: Caleb Szalacinski <opensource@skiboy.net>

--- proxynect/proxydaemon.c
+++ proxynect/proxydaemon.c
@@ -29,6 +29,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 #define log(level, ...) do { if(device->loglevel >= FREENECT_LOG_##level) { fprintf(stderr, __VA_ARGS__); } } while(0)
 
