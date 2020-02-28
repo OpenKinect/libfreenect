3e4f98bef4bef7c2bbe4ce7de644746506b1f25b
Nicolas Chauvet <kwizart@gmail.com>
2020-02-28 14:40:00 +0100

    Fix headers for opencv2 and later
    opencv/cv.h is removed on opencv4
    
    Signed-off-by: Nicolas Chauvet <kwizart@gmail.com>

--- wrappers/opencv/cvdemo.cpp
+++ wrappers/opencv/cvdemo.cpp
@@ -1,5 +1,4 @@
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <opencv2/highgui/highgui_c.h>
 #include <stdio.h>
 #include "libfreenect_cv.h"
 
--- wrappers/opencv/libfreenect_cv.h
+++ wrappers/opencv/libfreenect_cv.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <opencv/cv.h>
+#include <opencv2/core/core_c.h>
 
 #ifdef __cplusplus
 extern "C" {
